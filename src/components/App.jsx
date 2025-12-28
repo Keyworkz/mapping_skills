c47eb5193540af7a8738fab8cc47504e55bf60e4
Keyworkz Webdesign & Development <info@keyworkz.nl>
2025-12-28 20:21:15 +0100

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Card from "../components/Card";
+import emojipedia from "../emojipedia";
+
+// console.log(emojipedia);
+
+function createEntry(emojiObj) {
+  return (
+    <Card
+      key={emojiObj.id}
+      emoji={emojiObj.emoji}
+      name={emojiObj.name}
+      meaning={emojiObj.meaning}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+      <dl className="dictionary">{emojipedia.map(createEntry)}</dl>
+    </div>
+  );
+}
+
+export default App;
